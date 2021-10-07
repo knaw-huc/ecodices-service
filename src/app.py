4e7aa202537fabd542f0d73c1d36319e53da8e57
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2021-10-07 11:07:37 +0200

    Portainer ready

---
+++ src/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+import json
+from elastic_index import Index
+
+
+app = Flask(__name__)
+
+config = {
+    "url" : "ecodices_es",
+    "port" : "9200",
+    "doc_type" : "ecodices"
+}
+
+index = Index(config)
+
+
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+@app.route("/")
+def hello_world():
+    retStruc = {"app": "Procrustus service", "version": "0.1"}
+    return json.dumps(retStruc)
+
+@app.route("/facet", methods=['GET'])
+def get_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    ret_struc = index.get_facet(facet + ".keyword", amount)
+    return json.dumps(ret_struc)
+
+@app.route("/filter-facet", methods=['GET'])
+def get_filter_facet():
+    facet = request.args.get("name")
+    amount = request.args.get("amount")
+    facet_filter = request.args.get("filter")
+    ret_struc = index.get_filter_facet(facet + ".keyword", amount, facet_filter)
+    return json.dumps(ret_struc)
+
+@app.route("/browse", methods=['POST'])
+def browse():
+    struc = request.get_json()
+    ret_struc = index.browse(struc["page"], struc["page_length"], struc["sortorder"] + ".keyword", struc["searchvalues"])
+    return json.dumps(ret_struc)
+
+#Start main program
+
+if __name__ == '__main__':
+    app.run()
+
